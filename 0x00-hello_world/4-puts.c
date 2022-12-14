f59831e3352d409ca3dd946050d4ea9b5d5581fe
tundeola234 <117745897+tundeola234@users.noreply.github.com>
2022-12-14 10:43:20 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 # include <stdio.h>
 /**
-*main - main block
-*Return: o
+*main block
+*Return:o
 */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
+        puts("\"Programming is like building a multilingual puzzle")
         return (0);
 }
