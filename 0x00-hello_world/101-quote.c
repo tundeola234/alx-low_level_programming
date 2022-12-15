93799e3a69dfacf86fff3074cf6a183f6334dc02
tundeola234 <tunde050@gmail.com>
2022-12-15 04:27:22 -0800

    Unix is basically simple

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main -print a string of characters
+ *Return: 1
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
