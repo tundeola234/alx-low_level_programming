eafa86e037fcab6aadbf95b850c07e2e9b629664
tundeola234 <tunde050@gmail.com>
2022-12-19 04:15:22 -0800

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry for Genenrating random numbers
+* Return: Always 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	if (ch != 'e' && ch != 'q')
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
