8f2f707fca46afcf29b183991869dbe894d35359
tundeola234 <tunde050@gmail.com>
2022-12-19 08:15:45 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry for Genenrating random numbers
+* Return: Always 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
